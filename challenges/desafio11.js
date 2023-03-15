9ad435e73312b4c542e99e44a6453cf110e8763a
elderkampbell <elderkampbell@hotmail.com>
2023-03-15 16:27:38 -0300

    feat: challenge 13

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,4 +1,4 @@
 // Desafio 11
 // Retorne o nome do produto, a quantidade de curtidas e quantos itens foram vendidos dos produtos que não sejam iguais a Big Mac e McChicken
 db.produtos.find({ nome: { $nin: ["Big Mac", "McChicken"] } },
-    { nome: 1, vendidos: 1, curtidas: 1, _id: 0 });
+    { _id: 0, nome: 1, vendidos: 1, curtidas: 1 });
