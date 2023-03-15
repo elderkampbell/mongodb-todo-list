802431056db6a28cb3d3447310bf46c2b87ba1e4
elderkampbell <elderkampbell@hotmail.com>
2023-03-15 17:13:50 -0300

    feat: challenge 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,10 @@
+// Desafio 16
+// Adicione o campo ultimaModificacao com a data corrente somente no sanduíche Big Mac
+db.produtos.updateOne(
+    { nome: "Big Mac" },
+    { $currentDate: { ultimaModificacao: { $type: "date" } } },
+    );
+db.produtos.find(
+    { ultimaModificacao: { $exists: true } },
+    { _id: 0, nome: 1 },
+    );
