c5f02cfb2c53c06249c36727e8e3f29525f28b40
elderkampbell <elderkampbell@hotmail.com>
2023-03-15 17:17:59 -0300

    feat: challenge 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,6 @@
+// Desafio 17
+// Retorne a quantidade total de produtos em uma nova coleção chamada resumoProdutos
+db.resumoProdutos.insertOne(
+    { franquia: "McDonalds", totalProdutos: db.produtos.countDocuments({}) },
+    );
+db.resumoProdutos.find({}, { _id: 0 });
